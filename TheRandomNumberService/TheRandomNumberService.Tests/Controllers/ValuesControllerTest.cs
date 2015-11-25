076efe4d1aad763fbab28773741c2525ae3776e8
Thomas Sobieck <thomas@sobieck.com>
2015-11-25 07:47:03 -0600

    More changes.

--- TheRandomNumberService/TheRandomNumberService.Tests/Controllers/ValuesControllerTest.cs
+++ TheRandomNumberService/TheRandomNumberService.Tests/Controllers/ValuesControllerTest.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Web.Http;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TheRandomNumberService;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using TheRandomNumberService.Controllers;
 
 namespace TheRandomNumberService.Tests.Controllers
@@ -16,66 +10,11 @@ namespace TheRandomNumberService.Tests.Controllers
         [TestMethod]
         public void Get()
         {
-            // Arrange
-            ValuesController controller = new ValuesController();
+            var controller = new ValuesController();
 
-            // Act
-            IEnumerable<string> result = controller.Get();
+            var result = controller.Get().Result;
 
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(2, result.Count());
-            Assert.AreEqual("value1", result.ElementAt(0));
-            Assert.AreEqual("value2", result.ElementAt(1));
-        }
-
-        [TestMethod]
-        public void GetById()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            string result = controller.Get(5);
-
-            // Assert
-            Assert.AreEqual("value", result);
-        }
-
-        [TestMethod]
-        public void Post()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            controller.Post("value");
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Put()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            controller.Put(5, "value");
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Delete()
-        {
-            // Arrange
-            ValuesController controller = new ValuesController();
-
-            // Act
-            controller.Delete(5);
-
-            // Assert
+            Assert.AreEqual(2, result);
         }
     }
 }
