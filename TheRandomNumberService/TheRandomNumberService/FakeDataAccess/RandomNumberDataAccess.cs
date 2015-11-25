e84fbdaad3745ff0310f1cd41ff9c42fdc2acc73
Thomas Sobieck <thomas@sobieck.com>
2015-11-25 07:53:08 -0600

    random number.

---
+++ TheRandomNumberService/TheRandomNumberService/FakeDataAccess/RandomNumberDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TheRandomNumberService.FakeDataAccess
+{
+    public interface IRandomNumberDataAccess
+    {
+        Task<int> Get();
+    }
+
+    public class RandomNumberDataAccess : IRandomNumberDataAccess
+    {
+        private Random random;
+
+        public RandomNumberDataAccess()
+        {
+            random = new Random();
+        }
+
+        public async Task<int> Get()
+        {
+            var delay = random.Next(20, 200);
+
+            await Task.Delay(delay);
+
+            return random.Next();
+        }
+    }
+}
